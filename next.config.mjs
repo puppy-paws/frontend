66e84baea87b89cf50caaba8d37573ea5b297c43
Growing-Jiwoo <fbaa4412@naver.com>
2024-02-29 15:21:20 +0900

    build: vanilla-extract 환경 셋팅 및 global css 셋팅

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
+const withVanillaExtract = createVanillaExtractPlugin();
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/upload/:slug",
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:slug`, // Matched parameters can be used in the destination
+      },
+    ];
+  },
+};
+
+module.exports = withVanillaExtract(nextConfig);
