2a4245f0395031092c3f7880ac4eeea0db2569bd
Growing-Jiwoo <fbaa4412@naver.com>
2024-03-02 16:17:03 +0900

    style: 전체 공통 컴포넌트 스타일 코드 수정

--- src/app/(route)/community/layout.tsx
+++ src/app/(route)/community/layout.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from "react";
-import * as styles from "../main.css";
 
 type Props = { children: ReactNode };
 export default function Layout({ children }: Props) {
-  return <div className={styles.container}>{children}</div>;
+  return <>{children}</>;
 }
