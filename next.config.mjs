6bba426a4160e19c3e4a14552021e3b291ea8c27
Growing-Jiwoo <fbaa4412@naver.com>
2024-03-04 16:50:58 +0900

    feat: svgr/webpack 설치 및 적용

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,14 @@ const nextConfig = {
       },
     ];
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 
 export default withVanillaExtract(nextConfig);
