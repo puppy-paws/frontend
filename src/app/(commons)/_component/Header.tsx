a83463bfa5e26c1b0e9b18a6b0a743e1e306eefe
Growing-Jiwoo <fbaa4412@naver.com>
2024-03-04 16:53:29 +0900

    refactor: 반려견 구인 커뮤니티 디자인 적용

--- src/app/(commons)/_component/Header.tsx
+++ src/app/(commons)/_component/Header.tsx
@@ -6,49 +6,45 @@ import * as styles from "./_style/header.css";
 export default function Header() {
   const pathname = headers().get("x-pathname") || "";
   return (
-    <header className={styles.header}>
-      <Image
-        src={"/mainlogo.png"}
-        width={103}
-        height={31}
-        alt="profile"
-      ></Image>
-      <div className={styles.linkContainer}>
-        <Link
-          href="/community"
-          className={pathname.startsWith("/community") ? styles.activeLink : ""}
-        >
-          커뮤니티
-        </Link>
-        <Link
-          href="/dogstagram"
-          className={
-            pathname.startsWith("/dogstagram") ? styles.activeLink : ""
-          }
-        >
-          견스타그램
-        </Link>
+    <>
+      <header className={styles.header}>
+        <Image
+          src={"/mainlogo.png"}
+          width={103}
+          height={31}
+          alt="profile"
+        ></Image>
+
+        <div className={styles.menuContainer}>
+          <Link href={"/chat"}> 채팅 </Link>
+          <Link href={"/myInfo"}> 내 정보 </Link>
+          <Link href={"/signin"}> 로그인 </Link>
+        </div>
+      </header>
+      <div className={styles.subHeader}>
+        <div className={styles.linkContainer}>
+          <Link
+            href="/community"
+            className={
+              pathname.startsWith("/community")
+                ? styles.activeLinkString
+                : styles.linkString
+            }
+          >
+            커뮤니티
+          </Link>
+          <Link
+            href="/dogstagram"
+            className={
+              pathname.startsWith("/dogstagram")
+                ? styles.activeLinkString
+                : styles.linkString
+            }
+          >
+            견스타그램
+          </Link>
+        </div>
       </div>
-      <div className={styles.menuContainer}>
-        <Image
-          src={"/testimg.png"}
-          width={30}
-          height={30}
-          alt="profile"
-        ></Image>
-        <Image
-          src={"/testimg.png"}
-          width={30}
-          height={30}
-          alt="profile"
-        ></Image>
-        <Image
-          src={"/testimg.png"}
-          width={30}
-          height={30}
-          alt="profile"
-        ></Image>
-      </div>
-    </header>
+    </>
   );
 }
