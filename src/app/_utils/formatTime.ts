6b42d16c4b5e32ef68306b63750b4a17d9b6fe6a
Growing-Jiwoo <fbaa4412@naver.com>
2024-03-07 17:17:01 +0900

    refactor: formatTime return 값 수정

--- src/app/_utils/formatTime.ts
+++ src/app/_utils/formatTime.ts
@@ -1,15 +1,18 @@
 import dayjs, { Dayjs } from "dayjs";
 
-export function formatTime(createdTime: string): string {
+export const formatTime = (createdTime: string): string => {
   const createdTimeDayjs: Dayjs = dayjs(createdTime);
   const currentTime: Dayjs = dayjs();
   const diffMinutes: number = currentTime.diff(createdTimeDayjs, "minute");
+  const diffHours: number = currentTime.diff(createdTimeDayjs, "hour");
 
   if (diffMinutes < 1) {
     return "방금 전";
   } else if (diffMinutes <= 10) {
     return `${diffMinutes}분 전`;
+  } else if (diffHours < 24) {
+    return createdTimeDayjs.format("HH:mm");
   } else {
     return createdTimeDayjs.format("MM월 DD일 HH:mm");
   }
-}
+};
