0cc05cfe983aedc2479364ac1df021f2cbafc892
Growing-Jiwoo <fbaa4412@naver.com>
2024-04-04 21:28:52 +0900

    feat: 견스타그램 생성 UI 퍼블리싱

--- src/app/(commons)/_component/CompleteButton.tsx
+++ src/app/(commons)/_component/CompleteButton.tsx
@@ -1,22 +1,22 @@
-"use client";
-
+import * as React from "react";
 import * as styles from "./_style/completeButton.css";
 import { RecoilValue, useRecoilValue } from "recoil";
+import { emptyRecoilState } from "@/app/_store/commons/atoms";
 
 interface Props<T> {
-  recoilState: RecoilValue<T>;
+  recoilState?: RecoilValue<T>;
+  isValid?: boolean;
 }
 
-export default function CompleteButton<T>({ recoilState }: Props<T>) {
-  const convertedValues = useRecoilValue(recoilState);
+export default function CompleteButton<T>({ recoilState, isValid }: Props<T>) {
+  const convertedValues = useRecoilValue(recoilState || emptyRecoilState);
 
   const isAllTrue = Object.values(convertedValues).every(
     (value) => value === true
   );
 
-  const buttonClassName = isAllTrue
-    ? styles.activeButton
-    : styles.nonActiveButton;
+  const buttonClassName =
+    isAllTrue || isValid ? styles.activeButton : styles.nonActiveButton;
 
   return <button className={buttonClassName}>완료</button>;
 }
