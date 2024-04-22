d4b382499e55288f6dfc3d29302c89f586b63081
Growing-Jiwoo <fbaa4412@naver.com>
2024-04-22 19:56:39 +0900

    feat: return-fetch commons api 생성

--- src/app/_apis/commonsApi.ts
+++ src/app/_apis/commonsApi.ts
@@ -4,10 +4,6 @@ export const fetchExtended = returnFetch({
   baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
   headers: { Accept: "application/json" },
   interceptors: {
-    // request: async (args) => {
-    //   return args;
-    // },
-
     response: async (response) => {
       return response;
     },
