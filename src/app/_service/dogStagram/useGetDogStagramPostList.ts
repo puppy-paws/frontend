69329439679a9f4169360ef9d1d883772bad94eb
Growing-Jiwoo <fbaa4412@naver.com>
2024-04-23 16:08:58 +0900

    refactor: useQuery -> useInfiniteQuery 수정 구현

--- src/app/_service/dogStagram/useGetDogStagramPostList.ts
+++ src/app/_service/dogStagram/useGetDogStagramPostList.ts
@@ -1,19 +1,26 @@
 import { DogStagramPostListType } from "@/app/_types/dogStagram";
-import { QueryKey, useQuery } from "@tanstack/react-query";
+import {
+  InfiniteData,
+  QueryKey,
+  useInfiniteQuery,
+} from "@tanstack/react-query";
 import { getDogStagramPostList } from "../../_apis/dogStagram/getDogStagramPostList";
 
 export const useGetDogStagramPostList = () => {
-  const { data: dogStagramPostList } = useQuery<
+  const { data, fetchNextPage, hasNextPage, isFetching } = useInfiniteQuery<
     DogStagramPostListType[],
     Object,
-    DogStagramPostListType[],
-    QueryKey
+    InfiniteData<DogStagramPostListType[]>,
+    QueryKey,
+    number
   >({
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => lastPage.at(-1)?.id,
     queryKey: ["dogstagramPostList"],
     queryFn: getDogStagramPostList,
     staleTime: 60 * 1000,
     gcTime: 300 * 1000,
   });
 
-  return { dogStagramPostList };
+  return { data, fetchNextPage, hasNextPage, isFetching };
 };
