39fa2929ea8601833c9a208d1fe568c92c910539
Growing-Jiwoo <fbaa4412@naver.com>
2024-05-01 15:06:40 +0900

    refactor: utc 시간 변환 로직 추가 및 시간 format 변경

--- src/app/_utils/formatTime.ts
+++ src/app/_utils/formatTime.ts
@@ -1,18 +1,20 @@
 import dayjs, { Dayjs } from "dayjs";
+import utc from "dayjs/plugin/utc";
 
-export const formatTime = (createdTime: string): string => {
-  const createdTimeDayjs: Dayjs = dayjs(createdTime);
-  const currentTime: Dayjs = dayjs();
+dayjs.extend(utc);
+
+export const formatTime = (createdTime: string | Date): string => {
+  const createdTimeDayjs: Dayjs = dayjs(createdTime).utc();
+  const currentTime: Dayjs = dayjs().utc();
   const diffMinutes: number = currentTime.diff(createdTimeDayjs, "minute");
   const diffHours: number = currentTime.diff(createdTimeDayjs, "hour");
-
   if (diffMinutes < 1) {
     return "방금 전";
   } else if (diffMinutes <= 10) {
     return `${diffMinutes}분 전`;
   } else if (diffHours < 24) {
-    return createdTimeDayjs.format("HH:mm");
+    return createdTimeDayjs.format("hh:mm A");
   } else {
-    return createdTimeDayjs.format("MM월 DD일 HH:mm");
+    return createdTimeDayjs.format("YYYY. MM. DD");
   }
 };
