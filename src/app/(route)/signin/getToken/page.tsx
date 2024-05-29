bfb47ef7e196cb33416a8244e8abc1f7d21837d1
Growing-Jiwoo <fbaa4412@naver.com>
2024-05-29 17:21:58 +0900

    feat: 로그인 기능 및  리프레쉬 토큰 발급 기능 구현

---
+++ src/app/(route)/signin/getToken/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { ACCESS_TOKEN } from "@/app/_const/const";
+import storage from "@/app/_utils/localStorage";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function GetToken() {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router) {
+      const query = new URLSearchParams(window.location.search);
+      const accessToken = query.get("accessToken");
+      if (accessToken) {
+        storage.set(ACCESS_TOKEN, accessToken);
+        window.history.go(-2);
+      }
+    }
+  }, [router]);
+
+  return <></>;
+}
