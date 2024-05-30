99329fb2ac39a99d73471844b34768223142415b
Growing-Jiwoo <fbaa4412@naver.com>
2024-05-30 19:32:31 +0900

    feat: toastify 라이브러리 설치

--- src/app/(route)/layout.tsx
+++ src/app/(route)/layout.tsx
@@ -4,6 +4,8 @@ import Header from "../(commons)/_component/Header";
 import RecoilRootProvider from "../(commons)/_component/RecoilProvider";
 import RQProvider from "../(commons)/_component/RQProvider";
 import * as styles from "./main.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const metadata: Metadata = {
   title: "반려견 산책 도우미를 구인해보세요 !",
@@ -16,6 +18,7 @@ export default function Layout({ children, modal }: Props) {
     <div className={styles.container}>
       <RecoilRootProvider>
         <RQProvider>
+          <ToastContainer position="top-center" autoClose={1500} />
           <Header />
           {modal}
           {children}
