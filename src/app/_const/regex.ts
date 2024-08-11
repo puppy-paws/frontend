e0e9fb247c1946a2ec74d52369e374e333402c0f
Growing-Jiwoo <fbaa4412@naver.com>
2024-08-11 21:46:34 +0900

    refactor: 반려견 특징, 자랑포인트 정규식 수정

--- src/app/_const/regex.ts
+++ src/app/_const/regex.ts
@@ -3,7 +3,7 @@ export const regexPatterns = {
   nickname: /^[a-zA-Z0-9가-힣]{1,8}$/,
   dogName: /^[가-힣]{1,8}$/,
   dogBreed: /^[가-힣]{1,20}$/,
-  dogCharacter: /^[a-zA-Z0-9가-힣\s!?\(\)~.,“”'^*]{1,30}$/,
-  dogBoast: /^[a-zA-Z0-9가-힣\s!?\(\)~.,“”'^*]{1,30}$/,
+  dogCharacter: /^[a-zA-Z0-9가-힣ㄱ-ㅎㅏ-ㅣ\s!?\(\)~.,""'^*]{1,30}$/,
+  dogBoast: /^[a-zA-Z0-9가-힣ㄱ-ㅎㅏ-ㅣ\s!?\(\)~.,""'^*]{1,30}$/,
   dogPersonality: /^[가-힣]{1,5}$/,
 };
