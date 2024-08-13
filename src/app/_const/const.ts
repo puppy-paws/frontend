02d512ceb49e8de682d7e0c7bc8eb280442c34d9
Growing-Jiwoo <fbaa4412@naver.com>
2024-08-13 18:45:32 +0900

    feat: socket const 추가

--- src/app/_const/const.ts
+++ src/app/_const/const.ts
@@ -1,2 +1,8 @@
+import { io, Socket } from "socket.io-client";
+
 export const ACCESS_TOKEN = "accessToken";
 export const REFRESH_TOKEN = "refreshToken";
+
+export const socket: Socket = io(`${process.env.NEXT_PUBLIC_SOCKET_BASE_URL}`, {
+  transports: ["websocket"],
+});
